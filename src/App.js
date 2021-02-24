110147d68b8ae5d10159b7bc1b55659c1a3e717b
gabrielle-12 <55379118+gabrielle-12@users.noreply.github.com>
2021-02-24 14:34:38 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from "./components/Navbar"
+import Main from "./Main"
+
+class App extends React.Component {
+  render() {
+    return(
+      <div>
+        <Navbar />
+        <Main />
+      </div>
+    )
+  }
+}
+export default App;
---
+++ src/Main.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
+class Main extends React.Component {
+    render() {
+        return (
+    <Switch>
+        <Route exact path="/" component= {Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+    </Switch>
+        )
+    }
+}
+
+export default Main;
